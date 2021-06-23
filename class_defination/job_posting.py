6343afcb55efa1abdeab7b60c0040da5dc861317
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-06-22 23:08:24 -0400

    class definations for the postings

---
+++ class_defination/job_posting.py
@@ -0,0 +1,22 @@
+class job_posting_indeed(object):
+    def __init__(self):
+        self.job_title = None
+        self.company = None
+        self.original_link = None
+        self.is_full_time = None
+        self.is_remote = None
+        self.job_location = None
+
+
+class job_posting_linkedIn(object):
+    def __init__(self):
+        self.job_title = None
+        self.company = None
+        self.original_link = None
+        self.job_location = None
+        self.seniority_level = None
+        self.employment_type = None
+        self.job_function = None
+        self.industries = None
+        self.linkedIn_link = None
+        self.posted_data = None
