79aa13071c96af6ccced69d5f286a59e649b8c5d
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-07-03 19:26:28 -0400

    now handles invalid entry

--- scrapers/for_both.py
+++ scrapers/for_both.py
@@ -1,11 +1,28 @@
-from scrapers import indeed_scraper 
+from scrapers import indeed_scraper
 from scrapers import linkedIn_scraper
 
 def run():
-    print('\n')
-    job = input("\tEnter the job: ")
-    location = input("\tEnter the location: ")
-    print("\n\tWorking with Indeed scraper\n")
-    indeed_scraper.run(job, location)
-    print("\n\tWorking with LinkedIn scraper")
-    linkedIn_scraper.run(job, location)
+    while True:
+        try:
+            print('\n')
+            job = input("\tEnter the job: ")
+            location = input("\tEnter the location: ")
+            scrap = input('\n\n\t\tYou want to parse from? \n\tLinkedIn \t\tIndeed \t\tBoth\n\t\t Enter: ')
+            if  'both' in scrap.lower():
+                print("\n\n\tWorking with Indeed scraper\n")
+                indeed_scraper.run(job, location)
+                print("\n\n\tWorking with LinkedIn scraper")
+                linkedIn_scraper.run(job, location)
+            elif 'linkedin' in scrap.lower():
+                print("\n\n\tWorking with LinkedIn scraper")
+                linkedIn_scraper.run(job, location)
+            elif 'indeed' in scrap.lower():
+                print("\n\n\tWorking with Indeed scraper\n")
+                indeed_scraper.run(job, location)
+
+        except:
+            print("\n\nInvalid Entry; Try again")
+            continue
+
+if __name__ =='scrapers.for_both':
+    run()
